1061cfe3f49c92d17850f9053653ce4ca1f12980
josband <jro44@pitt.edu>
2022-04-20 23:31:37 -0400

    Game setup

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -1,6 +1,14 @@
 package chess;
 
+// THIS WILL BE IMPLEMENTED BEFORE LAST
 public class Game {
+    private Player whitePlayer;
+    private Player blackPlayer;
     private List<Move> whiteMoves;
+    private List<Piece> whitePieces;
     private List<Move> blackMoves;
+    private List<Piece> blackPieces;
+    private Board board;
+
+    
 }
