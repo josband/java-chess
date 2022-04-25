c4e21b71db9e15c029d2f770eb28f48c9cb6ddcf
josband <jro44@pitt.edu>
2022-04-24 23:40:52 -0400

    Added Queen File

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,11 @@
+package chess.pieces;
+
+public class Queen {
+    public Queen(Alliance alliance) {
+        super(alliance);
+    }
+
+    public List<Move> calculateLegalMoves(Board board, Tile start) {
+        
+    }
+}
