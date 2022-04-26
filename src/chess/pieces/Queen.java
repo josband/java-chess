3e22944a17ca5c1a1264c9281519840a491ef56e
josband <jro44@pitt.edu>
2022-04-25 21:04:53 -0400

    Queen Implementation

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -1,11 +1,21 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+
 public class Queen {
     public Queen(Alliance alliance) {
         super(alliance);
     }
 
     public List<Move> calculateLegalMoves(Board board, Tile start) {
-        
+        List<Move> queenMoves = new ArrayList<Move>();
+
+        checkRankAndFile(queenMoves, board, start);
+
+        checkDiagnol(queenMoves, board, start, 1, 1);
+        checkDiagnol(queenMoves, board, start, 1, -1);
+        checkDiagnol(queenMoves, board, start, -1, -1);
+        checkDiagnol(queenMoves, board, start, -1, 1);
+
     }
 }
