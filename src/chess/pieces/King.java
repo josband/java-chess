023c4f240d0056a12058f09e40a0eafe82762536
josband <jro44@pitt.edu>
2022-05-06 16:47:42 -0400

    Further king additions

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -4,12 +4,43 @@ import java.util.*;
 import chess.board.*;
 
 public class King extends Piece {
+    private boolean inCheck;
+
     public King(Alliance alliance, String imgPath) {
         super(alliance, imgPath);
+        inCheck = false;
+    }
+
+    public boolean setCheck(boolean check) {
+        return this.inCheck = check;
+    }
+
+    public boolean isChecked() {
+        return this.inCheck;
     }
 
     public List<Move> calculateLegalMoves(Board board, Tile start) {
         List<Move> kingMoves = new ArrayList<Move>();
+        int kingX, kingY;
+
+        kingX = start.getX();
+        kingY = start.getY();
+
+        for (int i = -1; i < 2; i++) {
+            try {
+                if (board.get(kingY + 1, kingX + i).getPiece() == null || board.get(kingY + 1, kingX + i).getPiece().getAlliance() != this.alliance) {
+                    kingMoves.add(new Move(start, board.get(kingY + 1, kingX + i)));
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+            }
+
+            try {
+                if (board.get(kingY - 1, kingX + i).getPiece() == null || board.get(kingY - 1, kingX + i).getPiece().getAlliance() != this.alliance) {
+                    kingMoves.add(new Move(start, board.get(kingY - 1, kingX + i)));
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {      
+            }
+        }
 
         return kingMoves;
     }
