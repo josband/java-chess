1a30467b57459cb0aebc782dd12b8046557260c7
josband <jro44@pitt.edu>
2022-05-06 16:48:28 -0400

    Started GUI

--- src/Chess.java
+++ src/Chess.java
@@ -1,5 +1,8 @@
+import chess.*;
+import chess.gui.*;
+
 public class Chess {
     public static void main(String[] args) {
-        
+        new GameFrame();
     }
 }
--- src/chess/gui/GameFrame.java
+++ src/chess/gui/GameFrame.java
@@ -1,11 +1,15 @@
 package chess.gui;
 
+import java.awt.*;
 import javax.swing.*;
+
+import chess.Game;
 
 // New Territory, will implement when there is more functionality
 public class GameFrame extends JFrame{
     public GameFrame() {
         super("Java Chess");
+        setBackground(Color.BLACK);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setSize(750, 750);
